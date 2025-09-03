c3aeb616ed8ad4a6c3ecd6576a6c7dac66bbc2f4
Dennis Dyall <dennis.dyall@yubico.com>
2025-09-04 01:18:18 +0200

    Add DimensionNumber property to SieObject

--- Skattata.Core/SieObject.cs
+++ Skattata.Core/SieObject.cs
@@ -2,6 +2,7 @@ namespace Skattata.Core;
 
 public class SieObject
 {
+    public string DimensionNumber { get; set; } = "";
     public string ObjectNumber { get; set; } = "";
     public string ObjectName { get; set; } = "";
 }
